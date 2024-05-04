90ff3604afefb1394109bff044287eb432e42f2d
zahy nadhir nashrullah <zahynadhir89@gmail.com>
2024-05-04 19:53:52 +0700

    a

--- latihan 8 Php/konversiTerbilang.php
+++ latihan 8 Php/konversiTerbilang.php
@@ -14,43 +14,48 @@
 <body>
 <h1>Halaman PHP zahy</h1>
  
-<?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $angka = $_POST['angka'];
+<form method="post">
+    Masukkan angka (1-9): <input type="text" name="angka"><br><br>
+    <input type="submit" value="Konversi">
+</form>
 
-        switch ($angka) {
-            case 1:
-                echo "Satu";
-                break;
-            case 2:
-                echo "Dua";
-                break;
-            case 3:
-                echo "Tiga";
-                break;
-            case 4:
-                echo "Empat";
-                break;
-            case 5:
-                echo "Lima";
-                break;
-            case 6:
-                echo "Enam";
-                break;
-            case 7:
-                echo "Tujuh";
-                break;
-            case 8:
-                echo "Delapan";
-                break;
-            case 9:
-                echo "Sembilan";
-                break;
-            default:
-                echo "Angka tidak valid.";
-        }
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $angka = $_POST['angka'];
+
+    switch ($angka) {
+        case 1:
+            echo "Satu";
+            break;
+        case 2:
+            echo "Dua";
+            break;
+        case 3:
+            echo "Tiga";
+            break;
+        case 4:
+            echo "Empat";
+            break;
+        case 5:
+            echo "Lima";
+            break;
+        case 6:
+            echo "Enam";
+            break;
+        case 7:
+            echo "Tujuh";
+            break;
+        case 8:
+            echo "Delapan";
+            break;
+        case 9:
+            echo "Sembilan";
+            break;
+        default:
+            echo "Angka tidak valid.";
     }
-    ?>
+}
+?>
 
 </body>
 </html>
